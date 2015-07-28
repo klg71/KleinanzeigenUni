ad098468e5b8ade13fd2bab885fbfab21f490bed
Lukas Meisegeier <lukas.meisegeier@vacutec-gmbh.de>
2015-07-28 16:06:48 +0200

    Added Offer Suggestion Command

---
+++ src/backend/commands/SuggestOffersCommand.java
@@ -0,0 +1,30 @@
+package backend.commands;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import backend.CategoryManager;
+import backend.Command;
+import backend.LoginManager;
+import backend.Offer;
+import backend.OfferManager;
+
+public class SuggestOffersCommand extends Command {
+
+	public SuggestOffersCommand(LoginManager loginManager, OfferManager offerManager, CategoryManager categoryManager) {
+		super(loginManager, offerManager, categoryManager);
+		keywords.add("suggest");
+		keywords.add("Suggest");
+		keywords.add("SUGGEST");
+	}
+
+	@Override
+	public void execute(ArrayList<String> parameters) {
+		for (Map.Entry<String, Offer> offer : offerManager.suggestOffers(currentUser)
+				.entrySet()) {
+			printSmallOffer(offer.getValue());
+		}
+
+	}
+
+}
