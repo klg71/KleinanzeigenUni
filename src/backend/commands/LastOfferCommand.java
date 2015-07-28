f291dab5a3dcd18166f26f620e7447344afcd686
Lukas Meisegeier <lukas.meisegeier@vacutec-gmbh.de>
2015-07-28 16:05:18 +0200

    Added Show Last Offers Command

---
+++ src/backend/commands/LastOfferCommand.java
@@ -0,0 +1,27 @@
+package backend.commands;
+
+import java.util.ArrayList;
+
+import backend.CategoryManager;
+import backend.Command;
+import backend.LoginManager;
+import backend.OfferManager;
+
+public class LastOfferCommand extends Command {
+
+	public LastOfferCommand(LoginManager loginManager, OfferManager offerManager, CategoryManager categoryManager) {
+		super(loginManager, offerManager, categoryManager);
+		keywords.add("last");
+		keywords.add("Last");
+		keywords.add("LAST");
+	}
+
+	@Override
+	public void execute(ArrayList<String> parameters) {
+		for (Integer offer : currentUser.getVisitedOffers()) {
+			printSmallOffer(offerManager.getOfferById(offer));
+		}
+
+	}
+
+}
