39f9ef88b599129441517cfdd0583042643be673
Lukas Meisegeier <lukas.meisegeier@vacutec-gmbh.de>
2015-07-28 16:04:52 +0200

    Added Enabling/Disabling of Offers

---
+++ src/backend/commands/DisableOfferCommand.java
@@ -0,0 +1,52 @@
+package backend.commands;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import backend.CategoryManager;
+import backend.Command;
+import backend.LoginManager;
+import backend.Offer;
+import backend.OfferManager;
+
+public class DisableOfferCommand extends Command {
+
+	public DisableOfferCommand(LoginManager loginManager, OfferManager offerManager, CategoryManager categoryManager) {
+		super(loginManager, offerManager, categoryManager);
+		keywords.add("disable");
+		keywords.add("Disable");
+		keywords.add("DISABLE");
+	}
+
+	@Override
+	public void execute(ArrayList<String> parameters) {
+		Integer OfferId = 0;
+		switch(parameters.size()){
+		case 0:
+			System.out.println("Please enter OfferId:");
+			try {
+				String input = br.readLine();
+				OfferId = Integer.parseInt(input);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		default:
+			OfferId=Integer.parseInt(parameters.get(0));
+		}
+		
+		Offer offer = offerManager.getOfferById(OfferId);
+		if (offer == null) {
+			System.out.println("This ID does not exist\n");
+			return;
+		}
+		if(currentUser.getId()!=offer.getId()){
+			System.out.println("You cant edit this!");
+			return;
+		}
+		offerManager.disableOffer(offer);
+		
+
+	}
+
+}
---
+++ src/backend/commands/EnableOfferCommand.java
@@ -0,0 +1,52 @@
+package backend.commands;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import backend.CategoryManager;
+import backend.Command;
+import backend.LoginManager;
+import backend.Offer;
+import backend.OfferManager;
+
+public class EnableOfferCommand extends Command {
+
+	public EnableOfferCommand(LoginManager loginManager, OfferManager offerManager, CategoryManager categoryManager) {
+		super(loginManager, offerManager, categoryManager);
+		keywords.add("enable");
+		keywords.add("Enable");
+		keywords.add("ENABLE");
+	}
+
+	@Override
+	public void execute(ArrayList<String> parameters) {
+		Integer OfferId = 0;
+		switch(parameters.size()){
+		case 0:
+			System.out.println("Please enter OfferId:");
+			try {
+				String input = br.readLine();
+				OfferId = Integer.parseInt(input);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		default:
+			OfferId=Integer.parseInt(parameters.get(0));
+		}
+		
+		Offer offer = offerManager.getOfferById(OfferId);
+		if (offer == null) {
+			System.out.println("This ID does not exist\n");
+			return;
+		}
+		if(currentUser.getId()!=offer.getId()){
+			System.out.println("You cant edit this!");
+			return;
+		}
+		offerManager.enableOffer(offer);
+		
+
+	}
+
+}
