2bdfa7848990fcc051d473b56aa077cd7bd41d81
klg71 <klg71@web.de>
2015-08-03 22:29:03 +0200

    improved suggestion functionality

--- src/backend/OfferManager.java
+++ src/backend/OfferManager.java
@@ -30,32 +30,33 @@ public class OfferManager {
 	}
 
 	public Offer addOffer(Offer newOffer) throws Exception {
-		Offer offer= databaseConnector.addOffer(newOffer);
+		Offer offer = databaseConnector.addOffer(newOffer);
 		if (!offers.containsKey(newOffer.getName())) {
-			offers.put(newOffer.getId(),offer);
+			offers.put(newOffer.getId(), offer);
 		} else {
 			throw new Exception("Offer Creation failed: Offername exists");
 		}
 		return offer;
 	}
-	
-	public void checkOffers(){
-		ArrayList<Offer>deletedOffers=new ArrayList<Offer>();
-		for(Map.Entry<Integer, Offer> entry : offers.entrySet()){
-			if(!entry.getValue().isAvailable()){
-				java.util.Date d=new java.util.Date();
-				d.setHours(d.getHours()-2);
-				if(entry.getValue().getTime().getTime()<d.getTime()){
+
+	public void checkOffers() {
+		ArrayList<Offer> deletedOffers = new ArrayList<Offer>();
+		for (Map.Entry<Integer, Offer> entry : offers.entrySet()) {
+			if (!entry.getValue().isAvailable()) {
+				java.util.Date d = new java.util.Date();
+				d.setHours(d.getHours() - 2);
+				if (entry.getValue().getTime().getTime() < d.getTime()) {
 					deletedOffers.add(entry.getValue());
 				}
 			}
 		}
-		for(Offer offer:deletedOffers){
+		for (Offer offer : deletedOffers) {
 			deleteOffer(offer);
 		}
 	}
 
-	public HashMap<Integer, Offer> searchOffers(String haystack, Integer category) {
+	public HashMap<Integer, Offer> searchOffers(String haystack,
+			Integer category) {
 		HashMap<Integer, Offer> retMap = new HashMap<Integer, Offer>();
 		if (category == 0) {
 			for (Map.Entry<Integer, Offer> entry : offers.entrySet()) {
@@ -66,16 +67,17 @@ public class OfferManager {
 			return retMap;
 		} else {
 			for (Map.Entry<Integer, Offer> entry : offers.entrySet()) {
-				if (entry.getValue().getName().contains(haystack)&&entry.getValue().getCategoryID()==category) {
+				if (entry.getValue().getName().contains(haystack)
+						&& entry.getValue().getCategoryID() == category) {
 					retMap.put(entry.getKey(), entry.getValue());
 				}
 			}
-			
+
 			return retMap;
 		}
 	}
-	
-	public void disableOffer(Offer offer){
+
+	public void disableOffer(Offer offer) {
 		offer.setAvailable(false);
 		try {
 			databaseConnector.editOffer(offer);
@@ -83,10 +85,10 @@ public class OfferManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void enableOffer(Offer offer){
+
+	public void enableOffer(Offer offer) {
 		offer.setAvailable(true);
 		try {
 			databaseConnector.editOffer(offer);
@@ -95,7 +97,7 @@ public class OfferManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Offer getOfferById(Integer offerId) {
 		for (Map.Entry<Integer, Offer> entry : offers.entrySet()) {
 			if (entry.getValue().getId() == offerId) {
@@ -104,9 +106,9 @@ public class OfferManager {
 		}
 		return null;
 	}
-	
-	public void editOffer(String oldname, Offer editedOffer){
-		offers.put(editedOffer.getId(),editedOffer);
+
+	public void editOffer(String oldname, Offer editedOffer) {
+		offers.put(editedOffer.getId(), editedOffer);
 		try {
 			databaseConnector.editOffer(editedOffer);
 		} catch (SQLException e) {
@@ -114,8 +116,8 @@ public class OfferManager {
 			e.printStackTrace();
 		}
 	}
-	
-	public void deleteOffer(Offer deletedOffer){
+
+	public void deleteOffer(Offer deletedOffer) {
 		offers.remove(deletedOffer.getId());
 		try {
 			databaseConnector.deleteOffer(deletedOffer);
@@ -126,7 +128,7 @@ public class OfferManager {
 	}
 
 	public HashMap<Integer, Offer> suggestOffers(User user) {
-		ArrayList<Offer> allOffers=new ArrayList<Offer>();
+		ArrayList<Offer> allOffers = new ArrayList<Offer>();
 		// Endgültiges Ergebnis
 		HashMap<Integer, Offer> returnMap = new HashMap<Integer, Offer>();
 
@@ -136,7 +138,8 @@ public class OfferManager {
 		}
 		// Füge alle aus Suchen hinzu
 		for (Search search : user.getSearches()) {
-			for(Map.Entry<Integer, Offer> entry:searchOffers(search.getSearchString(),search.getCategory()).entrySet()){
+			for (Map.Entry<Integer, Offer> entry : searchOffers(
+					search.getSearchString(), search.getCategory()).entrySet()) {
 				allOffers.add(entry.getValue());
 			}
 		}
@@ -146,36 +149,41 @@ public class OfferManager {
 
 		// Gehe durch alle Elemente durch
 		for (Offer entry : allOffers) {
-			if (orderedMap.containsKey(entry)) {
-				orderedMap.put(entry,
-						orderedMap.get(entry) + 1);
-			} else {
-				orderedMap.put(entry, 1);
+			if (entry != null) {
+				if (orderedMap.containsKey(entry)) {
+					orderedMap.put(entry, orderedMap.get(entry) + 1);
+				} else {
+					orderedMap.put(entry, 1);
+				}
 			}
 		}
 
 		// Häufigste nach oben sortieren
-		List<Map.Entry<Offer, Integer>> sortedList = new LinkedList<Map.Entry<Offer, Integer>>(
+		List<Map.Entry<Offer, Integer>> sortedList = new ArrayList<Map.Entry<Offer, Integer>>(
 				orderedMap.entrySet());
-		Collections.sort(sortedList,
-				new Comparator<Map.Entry<Offer, Integer>>() {
+		if (orderedMap.size() > 1) {
+			Collections.sort(sortedList,
+					new Comparator<Map.Entry<Offer, Integer>>() {
 
-					@Override
-					public int compare(Entry<Offer, Integer> o1,
-							Entry<Offer, Integer> o2) {
-						return o1.getValue() - o2.getValue();
-					}
-				});
+						@Override
+						public int compare(Entry<Offer, Integer> o1,
+								Entry<Offer, Integer> o2) {
+							return o1.getValue() - o2.getValue();
+						}
+					});
+		}
 
 		// Elemente zurückgeben
-		if (sortedList.size() < 3) {
-			for (Entry<Offer, Integer> Entry : sortedList) {
-				returnMap.put(Entry.getKey().getId(), Entry.getKey());
-			}
-		} else {
-			for (int i = 0; i < 3; i++) {
-				returnMap.put(sortedList.get(i).getKey().getId(), sortedList
-						.get(i).getKey());
+		if (sortedList.size() > 0) {
+			if (sortedList.size() < 3) {
+				for (Entry<Offer, Integer> entry : sortedList) {
+					returnMap.put(entry.getKey().getId(), entry.getKey());
+				}
+			} else {
+				for (int i = 0; i < 3; i++) {
+					returnMap.put(sortedList.get(i).getKey().getId(),
+							sortedList.get(i).getKey());
+				}
 			}
 		}
 		return returnMap;
